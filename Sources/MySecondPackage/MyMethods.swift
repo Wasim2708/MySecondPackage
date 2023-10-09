3b737da5e0c9c0e9f3392297b70c7cfe2567f140
wasim-18972 <wasim-18972@zohocorp.com>
2023-10-09 07:11:17 +0530

    Adding new mwthods

---
+++ Sources/MySecondPackage/MyMethods.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by wasim-18972 on 06/10/23.
+//
+
+import Foundation
+
+public class MyMethods {
+    public init() {}
+    
+    public func subtract(a: Int, b: Int) -> Int { a - b }
+}
